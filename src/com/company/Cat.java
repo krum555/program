3fd3966422a8bc727eba02fbd916c6c3e6078647
Krum Milanov <krum555.000@gmail.com>
2022-01-15 16:27:29 +0200

    Just example second commit

---
+++ src/com/company/Cat.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Cat implements IAnimal{
+    @Override
+    public void doit() {
+        System.out.println("MIAU");
+    }
+}
---
+++ src/com/company/Dog.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Dog implements IAnimal {
+    @Override
+    public void doit() {
+        System.out.println("BAU");
+    }
+}

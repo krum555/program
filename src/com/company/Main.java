a31a778b3b3c1c119677f052cb68c7227913ab7a
Krum Milanov <krum555.000@gmail.com>
2022-01-15 16:26:06 +0200

    Just example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        IAnimal animal = new Dog();
+        animal.doit();
+        animal = new Cat();
+        animal.doit();
     }
 }
